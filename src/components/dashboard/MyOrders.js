239199f385f1e8ff1ef013036e5ac79e7fb92b09
Shidul Islam <sishahin093@gmail.com>
2022-05-27 00:48:17 +0600

    order deelete complete

--- src/components/dashboard/MyOrders.js
+++ src/components/dashboard/MyOrders.js
@@ -3,12 +3,16 @@ import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link, useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
+import DeleteOrderModal from '../services/DeleteOrderModal';
+import OrderRow from './OrderRow';
 
 const MyOrders = () => {
     const [user] = useAuthState(auth);
     const navigate = useNavigate();
     const [appointment, setAppointment] = useState([]);
+    const [deleteOrder, setDeleteOrder] = useState(null);
     useEffect(() => {
+
         if (user) {
             fetch(`http://localhost:5000/booking?patient=${user?.email}`, {
                 method: 'GET',
@@ -35,7 +39,7 @@ const MyOrders = () => {
 
     return (
         <div>
-            <h2>My Appointments : {appointment.length}</h2>
+            <h2>My All Order : {appointment.length}</h2>
 
             <div class="overflow-x-auto">
                 <table class="table w-full">
@@ -51,29 +55,24 @@ const MyOrders = () => {
                     </thead>
                     <tbody>
                         {
-                            appointment.map((apoint, index) => <tr>
-                                <th>{index + 1}</th>
-                                <td>{apoint.serviceName}</td>
-                                <td>{apoint.quantity}</td>
-                                <td>$ {apoint.price}</td>
-                                <td>
-                                    {
-                                        apoint.paid &&
-                                        <button className='btn bg-red-300'>Delete</button>
-                                    }
-                                    </td>
-                                <td>
-                                    {
-
-                                        (apoint.price && !apoint.paid) && <Link to={`/dashboard/payment/${apoint._id}`}><button className='btn btn-xs btn-success'>Pay</button></Link>}
-
-                                    {(apoint.price && apoint.paid) && <span className='text-success'><button className='btn btn-xs btn-success'>Paid</button></span>}
-                                </td>
-                            </tr>)
+                            appointment.map((apoint, index) =>
+                                <OrderRow
+                                    index={index}
+                                    key={apoint._id}
+                                    apoint={apoint}
+                                    setDeleteOrder={setDeleteOrder}
+                                >
+                                </OrderRow>)
                         }
                     </tbody>
                 </table>
             </div>
+            {deleteOrder &&
+                <DeleteOrderModal
+                    deleteOrder={deleteOrder}
+                    setDeleteOrder={setDeleteOrder}
+                ></DeleteOrderModal>
+            }
         </div>
     );
 };
---
+++ src/components/dashboard/OrderRow.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+const OrderRow = ({ index, apoint, setDeleteOrder }) => {
+    return (
+        <>
+            <tr>
+                <th>{index + 1}</th>
+                <td>{apoint.serviceName}</td>
+                <td>{apoint.quantity}</td>
+                <td>$ {apoint.price}</td>
+                <td>
+                    {
+
+                        apoint.paid &&
+                        <label onClick={() => setDeleteOrder(apoint)} for="delete-confirm-modal" class="btn btn-xs btn-error"> Delete</label>
+                    }
+                </td>
+                <td>
+                    {
+
+                        (apoint.price && !apoint.paid) && <Link to={`/dashboard/payment/${apoint._id}`}><button className='btn btn-xs btn-success'>Pay</button></Link>
+                    }
+
+                    {
+                        (apoint.price && apoint.paid) && <span className='text-success'><button className='btn btn-xs btn-success'>Paid</button></span>
+                    }
+                </td>
+            </tr >
+        </>
+    );
+};
+
+export default OrderRow;
