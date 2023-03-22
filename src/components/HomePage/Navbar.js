cac637293cbd8540faf0737da22e1305d5ea4d7b
Shidul Islam <sishahin093@gmail.com>
2023-03-22 23:49:06 +0600

    some mistake

--- src/components/HomePage/Navbar.js
+++ src/components/HomePage/Navbar.js
@@ -54,16 +54,12 @@ const Navbar = () => {
                     {menuItems}
                 </ul>
             </div>
-            <div className="navbar-end">
-                {/* <label tabindex="1" for="dashboard-sidebar" className="btn btn-ghost ">
-                    <div class="avatar">
-                        <div class="w-20 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                            <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" alt='' />
-                        </div>
-                    </div>
-                </label> */}
-                {/* 
+            <div className="navbar-end lg:hidden">
+                <label tabindex="1" for="dashboard-sidebar" className="btn btn-ghost ">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                </label>
+                {/* 
+                    
                  */}
             </div>
         </div>
